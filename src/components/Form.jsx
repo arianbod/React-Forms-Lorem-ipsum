751c893e8ddd38c57ab309c3a41f8589275578c9
Arian <Arianbod@outlook.com>
2023-12-18 21:07:18 +0300

    Form implemented

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Form = () => {
-	return <div>Form</div>;
+const Form = ({ Quantity, handleChange, handleSubmit }) => {
+	return (
+		<form
+			className='lorem-form'
+			onSubmit={handleSubmit}>
+			<label htmlFor='number'>Paragraphs</label>
+			<input
+				id='number'
+				name='number'
+				type='number'
+				value={Quantity}
+				onChange={handleChange}
+				min='1'
+				step='1'
+				max='8'
+			/>
+			<button className='btn'>Generate</button>
+		</form>
+	);
 };
 
 export default Form;
--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const Text = () => {
-	return <div>Text</div>;
+const Text = ({ texts, Quantity = 1 }) => {
+	console.log(texts);
+	return (
+		<article className='lorem-text'>
+			{texts.map((item) => (
+				<p>{item}</p>
+			))}
+		</article>
+	);
 };
 
 export default Text;
