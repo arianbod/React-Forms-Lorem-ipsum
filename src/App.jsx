c44586b76f874e6f6190c1409d0a86f0a20d18f6
Arian <Arianbod@outlook.com>
2023-12-18 21:07:00 +0300

    Logic added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { useState } from 'react';
 import { Text, Title, Form } from './components';
+import textData from './data';
+
 const App = () => {
+	const [quantityState, setQuantityState] = useState(1);
+	const [textsState, setTextsState] = useState([]);
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('Form has been submitted');
+		setTextsState(textData.slice(0, quantityState));
+	};
+	const handleChange = (e) => {
+		setQuantityState(e.target.value > 1 ? e.target.value : 1);
+	};
 	return (
 		<>
 			<section className='section-center'>
 				<Title />
-				<Form />
-				<Text />
+				<Form
+					handleChange={handleChange}
+					handleSubmit={handleSubmit}
+					Quantity={quantityState}
+				/>
+				<Text
+					Quantity={quantityState}
+					texts={textsState}
+				/>
 			</section>
 		</>
 	);
